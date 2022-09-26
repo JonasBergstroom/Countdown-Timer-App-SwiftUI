872089853dde688baa8f466091742077fd2f4e78
jonasbergstrom <jontrian97@gmail.com>
2022-09-26 18:50:22 +0200

    Initial Commit

---
+++ Countdown-Timer-App-SwiftUI/Countdown_Timer_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countdown_Timer_App_SwiftUIApp.swift
+//  Countdown-Timer-App-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-09-26.
+//
+
+import SwiftUI
+
+@main
+struct Countdown_Timer_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
